24738f0168eaa3846a40cd9009615d67742d5326
David Day <dj6082013@gmail.com>
2024-11-01 05:27:27 +0000

    Add resnet notebook.

--- RLHF/train_resnet_image.py
+++ RLHF/train_resnet_image.py
@@ -1,22 +1,24 @@
+from argparse import ArgumentParser
 import os
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import models, transforms
-from torchvision.io import read_image
-from sklearn.metrics import roc_auc_score
-from tqdm import tqdm
-
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from torchmetrics.functional.classification import auroc
 from PIL import Image
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 class ImageDataset(Dataset):
-    def __init__(self, csv_path, img_dir, label_column, transform=None):
+    def __init__(self, csv_path, img_dir, label_column):
         self.df = pd.read_csv(csv_path)
         self.img_dir = img_dir
         self.label_column = label_column
-        self.transform = transform
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
     def __len__(self):
         return len(self.df)
@@ -29,88 +31,51 @@ class ImageDataset(Dataset):
             image = self.transform(image)
         return image, torch.tensor(label, dtype=torch.float32)
 
-# Define image transformations without downsampling
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-# Create Dataset and DataLoader for training and validation 
-train_dataset = ImageDataset('../data/train_downsampled_df.csv', '../data/image_folder_3lead', label_column='st_elevation', transform=transform)
-train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-
-test_dataset = ImageDataset('../data/test_downsampled_df.csv', '../data/image_folder_3lead', label_column='st_elevation', transform=transform)
-test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
-
 # Load ResNet model
-model = models.resnet18(pretrained=True)
-model.fc = torch.nn.Linear(model.fc.in_features, 1)  # Single label output for binary classification
+class ResNet(L.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = models.resnet18(pretrained=True)
+        self.model.fc = torch.nn.Linear(self.model.fc.in_features, 1)  # Single label output for binary classification
+        self.loss_fn = torch.nn.BCEWithLogitsLoss()
 
-# model = models.convnext_base(pretrained=True)
-# model.classifier[-1] = torch.nn.Linear(model.classifier[-1].in_features, 1)  # Single label output for binary classification
+    def forward(self, x):
+        # in lightning, forward defines the prediction/inference actions
+        return self.model(x)
 
-# Enable multi-GPU training if available
-if torch.cuda.device_count() > 1:
-    print(f"Using {torch.cuda.device_count()} GPUs")
-    model = torch.nn.DataParallel(model)
-
-model = model.to(device)
-
-# Save the first image for reference as PNG
-example_image, _ = train_dataset[0]
-example_image = transforms.ToPILImage()(example_image)
-example_image.save('example_resnet_image.png')
-
-# Loss and optimizer
-loss_fn = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-
-def train_one_epoch(model, dataloader, loss_fn, optimizer):
-    model.train()
-    total_loss = 0.0
-    total_samples = 0
-    for batch in dataloader:
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop. It is independent of forward
         inputs, labels = batch
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
         outputs = model(inputs).squeeze(1)
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item() * len(labels)
-        total_samples += len(labels)
-    return total_loss / total_samples
+        loss = self.loss_fn(outputs, labels)
+        self.log("train_loss", loss)
+        return loss
 
-def evaluate_model(model, dataloader):
-    model.eval()
-    total_loss = 0.0
-    total_samples = 0
-    all_labels = []
-    all_outputs = []
-    with torch.no_grad():
-        for batch in dataloader:
-            inputs, labels = batch
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs).squeeze(1)
-            loss = loss_fn(outputs, labels)
-            total_loss += loss.item() * len(labels)
-            total_samples += len(labels)
-            all_labels.extend(labels.cpu().numpy())
-            all_outputs.extend(torch.sigmoid(outputs).cpu().numpy())
-    avg_loss = total_loss / total_samples
-    auc = roc_auc_score(all_labels, all_outputs)
-    accuracy = ((torch.tensor(all_outputs) > 0.5).float() == torch.tensor(all_labels)).float().mean().item()
-    positive_share = (torch.tensor(all_outputs) > 0.5).float().mean().item()
-    return avg_loss, accuracy, auc, positive_share
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = model(inputs).squeeze(1)
+        loss = self.loss_fn(outputs, labels)
+        self.log("val_loss", loss)
+        self.log("val_auroc", auroc(outputs, labels.int(), task='binary'))
 
-# Training Loop
-n_epochs = 20
-for epoch in tqdm(range(n_epochs)):
-    train_loss = train_one_epoch(model, train_dataloader, loss_fn, optimizer)
-    val_loss, val_accuracy, val_auc, positive_share = evaluate_model(model, test_dataloader)
-    
-    # Print metrics
-    print(f"Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss:.4f}, "
-          f"Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}, "
-          f"Val AUC: {val_auc:.4f}, Positive Share: {positive_share:.4f}")
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=3e-4)
+        return optimizer
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--label_column', type=str, default='st_elevation')
+    args = parser.parse_args()
+
+    model = ResNet()
+
+    # Create Dataset and DataLoader for training and validation 
+    train_dataset = ImageDataset('../data/train_resnet_df.csv', '../data/image_folder', label_column=args.label_column)
+    train_dataloader = DataLoader(train_dataset, batch_size=64, num_workers=4, shuffle=True, pin_memory=True)
+
+    test_dataset = ImageDataset('../data/test_resnet_df.csv', '../data/image_folder', label_column=args.label_column)
+    test_dataloader = DataLoader(test_dataset, batch_size=64, num_workers=4, shuffle=False, pin_memory=True)
+
+    checkpoint_callback = ModelCheckpoint(monitor="val_loss")
+    trainer = L.Trainer(max_epochs=20, accelerator="auto", callbacks=[checkpoint_callback])
+    trainer.fit(model, train_dataloader, test_dataloader)
